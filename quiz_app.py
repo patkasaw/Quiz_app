17ca238e5e5994d99e86e0e7de0c0f0195b80a71
patkasaw <patrycja.sawicka52@gmail.com>
2025-05-07 18:19:29 +0200

    zapis wyników do pliku

--- quiz_app.py
+++ quiz_app.py
@@ -1,5 +1,7 @@
 import json
 import random
+from datetime import datetime
+
 
 with open('pytania.json', 'r', encoding = 'utf-8') as file:
     questions = json.load(file)
@@ -8,6 +10,9 @@ ilosc_pytan = 10
 wybrane_pytania = random.sample(questions, ilosc_pytan)
 
 punkty = 0
+
+imie = input('Podaj swoje imię').strip().lower()
+czas = datetime.now()
 
 for q in wybrane_pytania:
 
@@ -19,5 +24,14 @@ for q in wybrane_pytania:
     else:
         print(f"Źle! Poprawna odpowiedź to: {q["odpowiedz"]}")
 
-print(f"\nTwój wynik: {punkty} / {ilosc_pytan}")
+wynik = {punkty} / {ilosc_pytan}
+
+wiersz = f'{czas} | {imie} | {wynik} '
+
+print(f"\nTwój wynik: {wynik}")
+
+with open("wyniki.txt", "a", encoding="utf-8") as plik:
+    plik.write(wiersz)
+
+print("✅ Twój wynik został zapisany do pliku 'wyniki.txt'.")
 
